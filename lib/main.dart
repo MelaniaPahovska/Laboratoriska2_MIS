f7842112aa6f23f1f804af962b3e59eb9cb00c81
MelaniaPahovska <melania.pashovska@students.finki.ukim.mk>
2024-01-30 20:57:12 +0100

    laboratoriska 2 - 203082

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,304 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Clothing App 203082',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ClothingApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ClothingApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ClothingAppState createState() => _ClothingAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ClothingAppState extends State<ClothingApp> {
+  Map<String, List<String>> categorizedClothes = {
+    'Blouses': ['Item 1', 'Item 2'],
+    'Pants': ['Item 3', 'Item 4'],
+    'Shoes': ['Item 5'],
+    'Bags': ['Item 6', 'Item 7'],
+  };
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Map<String, Map<String, bool>> selectedClothes = {};
+  String selectedCategory = '';
+
+  @override
+  void initState() {
+    super.initState();
+    selectedCategory = categorizedClothes.keys.first;
+    _initializeSelection();
+  }
+
+  void _initializeSelection() {
+    categorizedClothes.forEach((category, items) {
+      selectedClothes[category] = {for (var item in items) item: false};
     });
+  }
+
+  void _showAddItemDialog() {
+    final _itemController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setStateDialog) {
+            return AlertDialog(
+              title: Text('Add Item', style: TextStyle(color: Colors.blue)),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    DropdownButton<String>(
+                      value: selectedCategory,
+                      onChanged: (String? newValue) {
+                        setStateDialog(() {
+                          selectedCategory = newValue ?? selectedCategory;
+                        });
+                      },
+                      items: categorizedClothes.keys
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child:
+                              Text(value, style: TextStyle(color: Colors.blue)),
+                        );
+                      }).toList(),
+                    ),
+                    TextField(
+                      controller: _itemController,
+                      decoration: InputDecoration(hintText: 'Item Name'),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel', style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('Add', style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    setState(() {
+                      String newItem = _itemController.text;
+                      if (newItem.isNotEmpty) {
+                        categorizedClothes[selectedCategory]?.add(newItem);
+                        selectedClothes[selectedCategory]?[newItem] = false;
+                      }
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _addClothing() {
+    _showAddItemDialog();
+  }
+
+  void _removeClothing() {
+    setState(() {
+      selectedClothes.forEach((category, items) {
+        var selectedItems = items.entries.where((item) => item.value).toList();
+        for (var selectedItem in selectedItems) {
+          categorizedClothes[category]?.remove(selectedItem.key);
+          selectedClothes[category]?.remove(selectedItem.key);
+        }
+      });
+    });
+  }
+
+  void _editClothing() {
+    String? editCategory = selectedCategory;
+    String? originalItem;
+    final _itemController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setStateDialog) {
+            return AlertDialog(
+              title: Text('Edit Item', style: TextStyle(color: Colors.blue)),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    DropdownButton<String>(
+                      value: editCategory,
+                      onChanged: (String? newValue) {
+                        setStateDialog(() {
+                          editCategory = newValue;
+                          if (categorizedClothes.containsKey(editCategory)) {
+                            originalItem =
+                                categorizedClothes[editCategory!]?.first;
+                          } else {
+                            originalItem = null;
+                          }
+                        });
+                      },
+                      items: categorizedClothes.keys
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child:
+                              Text(value, style: TextStyle(color: Colors.blue)),
+                        );
+                      }).toList(),
+                    ),
+                    DropdownButton<String>(
+                      hint: Text('Select an item',
+                          style: TextStyle(color: Colors.blue)),
+                      value: originalItem,
+                      onChanged: (String? newValue) {
+                        setStateDialog(() {
+                          originalItem = newValue;
+                          _itemController.text = newValue ?? '';
+                        });
+                      },
+                      items: (categorizedClothes[editCategory ?? ''] ?? [])
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child:
+                              Text(value, style: TextStyle(color: Colors.blue)),
+                        );
+                      }).toList(),
+                    ),
+                    TextField(
+                      controller: _itemController,
+                      decoration: InputDecoration(hintText: 'New Item Name'),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel', style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('Save', style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    if (editCategory != null &&
+                        originalItem != null &&
+                        _itemController.text.isNotEmpty) {
+                      setState(() {
+                        categorizedClothes[editCategory!]!
+                            .remove(originalItem!);
+                        categorizedClothes[editCategory!]!
+                            .add(_itemController.text);
+                        selectedClothes[editCategory!]!.remove(originalItem!);
+                        selectedClothes[editCategory!]![_itemController.text] =
+                            false;
+                      });
+                      Navigator.of(context).pop();
+                    } else {
+                      // Show an error message
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Error',
+                                style: TextStyle(color: Colors.red)),
+                            content: Text('Please fill in all fields.'),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('OK',
+                                    style: TextStyle(color: Colors.blue)),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title:
+            Text('Clothing App 203082', style: TextStyle(color: Colors.blue)),
+        backgroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: buildClothingList(),
+      floatingActionButton: buildActionButtons(),
+    );
+  }
+
+  ListView buildClothingList() {
+    return ListView.builder(
+      itemCount: categorizedClothes.keys.length,
+      itemBuilder: (context, index) {
+        String category = categorizedClothes.keys.elementAt(index);
+        return ExpansionTile(
+          title: Text(category),
+          children: categorizedClothes[category]!
+              .map((item) => CheckboxListTile(
+                    title: Text(item),
+                    value: selectedClothes[category]![item]!,
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        selectedClothes[category]![item] = newValue!;
+                      });
+                    },
+                  ))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Row buildActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildActionButton(Icons.add, 'Add', _addClothing),
+        _buildActionButton(Icons.remove, 'Remove', _removeClothing),
+        _buildActionButton(Icons.edit, 'Edit', _editClothing),
+      ],
+    );
+  }
+
+  Widget _buildActionButton(
+      IconData icon, String label, VoidCallback onPressed) {
+    return ElevatedButton.icon(
+      icon: Icon(icon, color: Colors.red),
+      label: Text(label, style: TextStyle(color: Colors.red)),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(primary: Colors.green),
     );
   }
 }
